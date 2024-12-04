ca900683eeb6d991d70cd3601a95adf9f7f72913
Adam Klisiewicz <adam.klisiewicz08@gmail.com>
2024-12-04 09:41:20 +0000

    pawelll

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,4 +1,28 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static MSG: RefCell<String> = RefCell::new(String::new());
+}
+
+
+
+#[ic_cdk::update]
+fn set_msg(name: String) -> String {
+   // new_msg : String
+   // msg.borrow_mut() = &mut String
+   // &mut String == String
+   MSG.with(|msg|{
+    msg.borrow_mut()= new_msg
+   })
+}
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
+
+#[ic_cdk::query]
+fn get.msg() -> String {
+    MSG.with(|msg|{ msg.borrow().clone()})
+}
+
